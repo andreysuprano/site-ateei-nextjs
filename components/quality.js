b3e22c2e7489cc060109bdc369e845a512f9fdaa
Andrey Suprano <andsuprano@gmail.com>
2022-06-07 17:07:07 -0300

    add isos link

--- components/quality.js
+++ components/quality.js
@@ -27,19 +27,21 @@ const Quality = () => {
               <div className="about-one__box">
                 <i className="besit-icon-medal"></i>
                 <div className="about-one__box-content">
-                  <h3><u><a style={{color:'#182c44'}}  href='https://downloads.ateei.com.br/ISOS_CERTIFICATES/ISO%209001%20%282021%29.pdf'>ISO 9001:2015 ⧉</a></u></h3>
+                  <h3>ISO 9001:2015</h3>
                   <p>
                   Possuímos excelência em nossos processos, garantindo que eles estão sendo gerenciados e em constante evolução.
                   </p>
+                  <u><a style={{color:'#182c44'}}  href='https://downloads.ateei.com.br/ISOS_CERTIFICATES/ISO%209001%20%282021%29.pdf'>Ver certificado ⧉</a></u>
                 </div>
               </div>
               <div className="about-one__box">
                 <i className="besit-icon-medal"></i>
                 <div className="about-one__box-content">
-                <h3><u><a style={{color:'#182c44'}} href='https://downloads.ateei.com.br/ISOS_CERTIFICATES/ISO%2013485%20%282021%29.pdf'>ISO 13485:2016 ⧉</a></u></h3>
+                <h3>ISO 13485:2016</h3>
                   <p>
                   Possuímos excelência na produção de itens médicos/hospitalares, garantindo que seu produto médico está sendo produzido com os critérios mínimos regulamentados para a fabricação de itens de tal importância.
                   </p>
+                  <u><a style={{color:'#182c44'}} href='https://downloads.ateei.com.br/ISOS_CERTIFICATES/ISO%2013485%20%282021%29.pdf'>Ver certificado ⧉</a></u>
                 </div>
               </div>
             </div>
