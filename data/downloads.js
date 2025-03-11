4ca67ea479d67f821e948bf50eebfecd1b57ab89
Andrey Suprano <54143276+andreysuprano@users.noreply.github.com>
2025-03-11 14:53:52 -0300

    Update downloads.js

--- data/downloads.js
+++ data/downloads.js
@@ -1,161 +1,182 @@
 export const DOWNLOADS = [
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: 'Manual de Instruções ME3011B – Português',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20ME3011b_P_r%2009%20at.pdf'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: ' Manual de Instruções ME3011B – English',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20ME3011b_E_r%2007%20at.pdf'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: 'Manual de Instruções ME3011b – Español',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20ME3011b_S_r08a%20at.pdf'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: 'DNP Slave Profile Mauell ME3011b',
-		link:
-			'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20Instru%c3%a7%c3%b5es%20_Hardware.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: ' Manual do tradutor SYNC2000_S4R1OEM_ES',
-		link: 'https://downloads.ateei.com.br/uploads/2017/02/Manual_do_tradutor_SYNC2000_S4R1OEM_ES.pdf'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: 'Manual e.Tool ME3011 config 4.02.',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20e.Tool%20config.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: ' Manual e.Tool ME3011 e.view 1.04.2',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/Manual%20e.Tool%20e.view.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: 'Manual e.Tool View ME3011B SW',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20e.Tool%20view.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: 'Manual de Configuração ModBus para ME3011B',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/Manual%20Modbus_.rar'
-	},
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: 'Manual de Instruções ME3011B – Português',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20ME3011b_P_r%2009%20at.pdf'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: ' Manual de Instruções ME3011B – English',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20ME3011b_E_r%2007%20at.pdf'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: 'Manual de Instruções ME3011b – Español',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20ME3011b_S_r08a%20at.pdf'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: 'DNP Slave Profile Mauell ME3011b',
+        link:
+            'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20Instru%c3%a7%c3%b5es%20_Hardware.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: ' Manual do tradutor SYNC2000_S4R1OEM_ES',
+        link: 'https://downloads.ateei.com.br/uploads/2017/02/Manual_do_tradutor_SYNC2000_S4R1OEM_ES.pdf'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: 'Manual e.Tool ME3011 config 4.02.',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20e.Tool%20config.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: ' Manual e.Tool ME3011 e.view 1.04.2',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual%20e.Tool%20e.view.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: 'Manual e.Tool View ME3011B SW',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual%20e.Tool%20view.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: 'Manual de Configuração ModBus para ME3011B',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual%20Modbus_.rar'
+    },
 
-	//SW
-	{
-		produto: 'ME3011B',
-		tipo: 's',
-		nome: 'Software e.Tool ME3011B Config 4.07.3',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/e.Tool%20ME3011%20config%204.07.3.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 's',
-		nome: 'Software Easy Connect DNP3 v4.3.0 – até/until 12/09/2021',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/easyconnect_4.3.0_%28dnp3%29.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 's',
-		nome: 'Software Easy Connect DNP3 – v5.7.1 – depois/after 12/09/2021',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/EasyConnect_V5.7.1_Setup.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 's',
-		nome: 'Software e.Tool ME3011B e.view 1.04.3',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/e.tool_me3011_e.view_1.04.3.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 's',
-		nome: 'Software ME3011 View SW',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/me3011_View_SW_setup.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 's',
-		nome: 'Software ME3011 View SW',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/me3011_View_SW_setup.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 's',
-		nome: 'Software ME3011 View SW',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/me3011_View_SW_setup.rar'
-	},
-	{
-		produto: 'ME3011B',
-		tipo: 'm',
-		nome: 'Página configurada para KIT ASK',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/Label%20Ask.rar'
-	},
+    //SW
+    {
+        produto: 'ME3011B',
+        tipo: 's',
+        nome: 'Software e.Tool ME3011B Config 4.07.3',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/e.Tool%20ME3011%20config%204.07.3.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 's',
+        nome: 'Software Easy Connect DNP3 v4.3.0 – até/until 12/09/2021',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/easyconnect_4.3.0_%28dnp3%29.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 's',
+        nome: 'Software Easy Connect DNP3 – v5.7.1 – depois/after 12/09/2021',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/EasyConnect_V5.7.1_Setup.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 's',
+        nome: 'Software e.Tool ME3011B e.view 1.04.3',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/e.tool_me3011_e.view_1.04.3.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 's',
+        nome: 'Software ME3011 View SW',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/me3011_View_SW_setup.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 's',
+        nome: 'Software ME3011 View SW',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/me3011_View_SW_setup.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 's',
+        nome: 'Software ME3011 View SW',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/me3011_View_SW_setup.rar'
+    },
+    {
+        produto: 'ME3011B',
+        tipo: 'm',
+        nome: 'Página configurada para KIT ASK',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Label%20Ask.rar'
+    },
 
-	//Antron M
-	{
-		produto: 'ANTRON',
-		tipo: 'm',
-		nome: 'Manual Antron IIs ',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual_Antron_IIs.rar'
-	},
-	{
-		produto: 'ANTRON',
-		tipo: 'm',
-		nome: 'Manual Implementação de Protocolo Modbus – Antron II-S',
-		link: 'https://downloads.ateei.com.br/uploads/2017/02/IMPLEMENTAÇÃO-DE-PROTOCOLO-MODBUS-ANTRON-II.pdf'
-	},
-	{
-		produto: 'ANTRON',
-		tipo: 'm',
-		nome: 'Manual de Operação do Software Antron II-s .NET2.0',
-		link: 'https://downloads.ateei.com.br/uploads/2017/02/Manual-SW-Antron-2.0-PT.pdf'
-	},
+    //Antron M
+    {
+        produto: 'ANTRON',
+        tipo: 'm',
+        nome: 'Manual Antron IIs ',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/MANUAIS_ME/Manual_Antron_IIs.rar'
+    },
+    {
+        produto: 'ANTRON',
+        tipo: 'm',
+        nome: 'Manual Implementação de Protocolo Modbus – Antron II-S',
+        link: 'https://downloads.ateei.com.br/uploads/2017/02/IMPLEMENTAÇÃO-DE-PROTOCOLO-MODBUS-ANTRON-II.pdf'
+    },
+    {
+        produto: 'ANTRON',
+        tipo: 'm',
+        nome: 'Manual de Operação do Software Antron II-s .NET2.0',
+        link: 'https://downloads.ateei.com.br/uploads/2017/02/Manual-SW-Antron-2.0-PT.pdf'
+    },
 
-	//Antron SW
-	{
-		produto: 'ANTRON',
-		tipo: 's',
-		nome: 'Download do Software Antron IIs – WIN 7/8',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/antron_vb.net_2013_1.0.5_windows_7-8.zip'
-	},
-	{
-		produto: 'ANTRON',
-		tipo: 's',
-		nome: 'Download do Software Antron IIs WinXP',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/Software%20AntronWindows%20Xp.rar'
-	},
+    //Antron SW
+    {
+        produto: 'ANTRON',
+        tipo: 's',
+        nome: 'Download do Software Antron IIs – WIN 7/8',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/antron_vb.net_2013_1.0.5_windows_7-8.zip'
+    },
+    {
+        produto: 'ANTRON',
+        tipo: 's',
+        nome: 'Download do Software Antron IIs WinXP',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Software%20AntronWindows%20Xp.rar'
+    },
 
-	//Antron SW
-	{
-		produto: 'AT160',
-		tipo: 'm',
-		nome: 'Manual do Carregador de Bateria Modelo AT160 FRN',
-		link: 'https://downloads.ateei.com.br/uploads/2020/Manual_Carregador_Baterias_AT160FRN-07.pdf'
-	},
-	{
-		produto: 'AT10',
-		tipo: 'm',
-		nome: 'Manual Fonte de Alimentação Modelo AT10 FRN',
-		link: 'https://downloads.ateei.com.br/uploads/2020/Manual%20AT10%20FRN%20Rev%200.pdf'
-	},
-	{
-		produto: 'MA2016b',
-		tipo: 'm',
-		nome: 'Manual de Instruções MA2016B',
-		link: 'https://downloads.ateei.com.br/uploads/downloads/Manual_MA2016/Manual%20MA%2018%20v04.pdf'
-	}
+    //Antron SW
+    {
+        produto: 'AT160',
+        tipo: 'm',
+        nome: 'Manual do Carregador de Bateria Modelo AT160 FRN',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual Carregador de Baterias AT160FRN Rev 07.pdf'
+    },
+    {
+        produto: 'AT160',
+        tipo: 'm',
+        nome: 'Manual do Carregador de Bateria Modelo AT160 FRN',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual Carregador de Baterias AT160FRN Rev 08 es.pdf'
+    },
+
+    {
+        produto: 'AT10',
+        tipo: 'm',
+        nome: 'Manual Fonte de Alimentação Modelo AT10 FRN',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual%20AT10%20FRN%20Rev%200.pdf'
+    },
+    {
+        produto: 'AT10',
+        tipo: 'm',
+        nome: 'Manual Fonte de Alimentação Modelo AT10 FRN - Espanhol',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual AT10 FRN Rev01 ES.pdf'
+    },
+    {
+        produto: 'MA2016b',
+        tipo: 'm',
+        nome: 'Manual de Instruções MA2016B',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual_MA2016/Manual%20MA%2018%20v04.pdf'
+    }
+    ,
+    {
+        produto: 'MA2016b',
+        tipo: 'm',
+        nome: 'Manual de Instruções MA2016B',
+        link: 'https://downloads.ateei.com.br/uploads/downloads/Manual MA 2016B v07 - es.pdf'
+    }
+
 ];
